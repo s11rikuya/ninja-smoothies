554008f642ba59074ff44e63b9f6011c50a41fae
Rikuya Suzuki <s11rikuya@gmail.com>
2019-09-19 00:04:38 +0900

    make Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link :to="{ name: '', params: {} }">
+          <span class="nav-title">Ninja Smoothies</span>
+        </router-link>
+        <a href="#" class="btn-floating btn-large halfway-fab pink">
+          <router-link :to="{ name: '', params: {} }">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+
+      </div>
+
+    </nav>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return  {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.navbar nav {
+  padding: 0px 20px
+}
+</style>
