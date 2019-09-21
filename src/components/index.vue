ad876138246a0a6ab8202e5679ff274e42f7660c
Rikuya Suzuki <s11rikuya@gmail.com>
2019-09-21 15:56:21 +0900

    add new function to delete some data in local

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,6 +2,7 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
         <h2 class="indigo-text">{{ smoothie.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
@@ -27,6 +28,13 @@ export default {
         { title: 'Moning Mood', slug: 'moning-mood', ingredients: ['mango', 'lime', 'juice'], id: '2' }
       ]
     }
+  },
+  methods: {
+    deleteSmoothie(id){
+      this.smoothies = this.smoothies.filter(smoothie => {
+        return smoothie.id !=  id
+      })
+    }
   }
 }
 </script>
@@ -49,5 +57,13 @@ export default {
 }
 .index .ingredients li {
   display: inline-block;
+}
+.index .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
 }
 </style>
